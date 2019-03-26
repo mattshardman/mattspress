c3cfa3e8607cb4dc7024862612793413e9268306
mattshardman <mattshardman@gmail.com>
2019-03-26 09:14:38 +0000

    refactor send methods add createRouter function

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const Mattspress = require('./lib/mattspress');
+const { Mattspress, CreateRouter } = require("./lib/mattspress");
+const { router, routeHandler } = require("./lib/router");
 
 const mattspress = () => new Mattspress();
 
+const Router = () => new CreateRouter();
+
 exports = module.exports = mattspress;
+exports.Router = Router;
